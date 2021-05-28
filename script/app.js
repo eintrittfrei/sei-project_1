6880c26828c9929a7a49859d515a7214e12cac13
Ole Nascimento <olenascimento@gmail.com>
2021-05-28 14:52:54 +0200

    project1

--- script/app.js
+++ script/app.js
@@ -3,33 +3,45 @@ function init() {
 
   // Grid
   const grid = document.querySelector('.grid')
-  console.log(grid)
+  //console.log(grid)
   const width = 40
-  console.log(width)
+  //console.log(width)
   const cellCount = width * width
-  console.log(cellCount)
+  //console.log(cellCount)
   const cells = []
-  console.log(cells)
+  //console.log(cells)
 
   // Lipstick shooter position 
   const lipClass = 'lipstick'
-  const lipStartPosition = 0
-  let lipCurrentPosition = 0
+  const lipStartPosition = 1573 // startign posititon of lipstick 
+  let lipCurrentPosition = 0 // current postition of lipstick 
 
-  
+
+  //The Grid
   for (let i = 0; i < cellCount; i++) {
-    console.log(cells)
+    //console.log(cells)
     const cell = document.createElement('div')
     cell.innerText = i
     grid.appendChild(cell)
     cells.push(cell)
 
+
+  }
+  //Add Lipstick to grid 
+  function addLipstick(position) {
+    console.log('position passed in', position)
+    cells[position].classList.add(lipClass)
+  }
+
+  // Remove lipstick from Grid
+  function removeLip(position) {
+    cells[position].classList.remove(lipClass)
   }
 
 
+  addLipstick(lipStartPosition)
 
-
-
+  console.log(addLipstick)
 
 
 
