e4ef0cf1ecba62d6975911908e4477707d685945
Ole Nascimento <olenascimento@gmail.com>
2022-02-20 17:22:23 +0100

    controller working

--- script/app.js
+++ script/app.js
@@ -1,9 +1,14 @@
 function init() {
   console.log('js up and running')
 
+  // GRID CLASS 
+
   class Grid {
     constructor(width) {
       this.width = width
+    }
+    getGridWidth() {
+      return this.width
     }
     selectDivForGrid() {
       const grid = document.querySelector('.grid')
@@ -27,17 +32,37 @@ function init() {
     }
   } // End of class 
 
+  // VARIABLES 
+
   const playGrid = new Grid(10)
   const gameArray = playGrid.makeGrid()
-  console.log('GAME ARRAY', gameArray)
-  const width = gameArray.length
+  const width = playGrid.getGridWidth()
+
+  const lipStartPosition = 84
+  let lipCurrentPosition = 84
+  const lipClass = 'lipstick'
+
+  let leftPressed = false
+  let rightPressed = false
+
+  const startButton = document.querySelector('#start')
+  const scoreCounter = document.querySelector('#points-display')
+  const livesCounter = document.querySelector('#lives-display')
+  let score = 0
+  let lives = 3
+
+  const dragClass = 'drags'
+  const dragStartposition = 10
+  let dragCurrentPosition = 10
+  let dragTimer
+  
+  const bullet = 'bullet'
+  let bulletCurrentPosition = lipCurrentPosition - 20
+  let bulletTimer
 
   
 
-
-  const lipStartPosition = 84
-  let lipCurrentPosition = 84 // current postition of vaccine 
-  const lipClass = 'lipstick'
+  // GAME CONTROLLER
 
   function addLipstick(position) {
     gameArray[position].classList.add(lipClass)
@@ -49,9 +74,6 @@ function init() {
   }
   addLipstick(lipStartPosition) 
   
-  
-  let leftPressed = false
-  let rightPressed = false
 
   function keyDownHandler(event) {
     if (event.keyCode === 39) {
@@ -76,8 +98,10 @@ function init() {
   function Controller() {
     removeLip(lipCurrentPosition)
     if (leftPressed && lipCurrentPosition % width !== 0) {
+      console.log(lipCurrentPosition)
       lipCurrentPosition--
     } else if (rightPressed && lipCurrentPosition % width !== width - 1) {
+      console.log(lipCurrentPosition)
       lipCurrentPosition++
     } else {
       //console.log('InvalidKey')
@@ -86,194 +110,168 @@ function init() {
     addLipstick(lipCurrentPosition)
   }
 
+ 
+  // VIRUS CONTROLS
 
+  function addDrags(position) {
+    gameArray[position].classList.add(dragClass)
+  }
+  function removeDrags(position) {
+    gameArray[position].classList.remove(dragClass)
+  }
+  addDrags(dragStartposition)
+  let trueOrFalse = true
+  let shootingBullet = false
+  function startGame() {
+    resetPoints()
+    startButton.disabled = true
 
-
-  // // const squaresGrid = document.querySelectorAll('.grid') // select the divs in the grid 
-  const startButton = document.querySelector('#start') // select start button 
-  const scoreCounter = document.querySelector('#points-display')
-  const livesCounter = document.querySelector('#lives-display')
-  let score = 0
-  let lives = 3
-  // // AUDIO
-  // // variables for aliens/ DRAGS
-  // // Virus
-  // const dragClass = 'drags'
-  // const dragStartposition = 10
-  // let dragCurrentPosition = 10
-  // let dragTimer
-  
-  // const bullet = 'bullet'
-  // // const bulletStart = lipCurrentPosition - 10 // inside function addBullet()
-  // let bulletCurrentPosition = lipCurrentPosition - 20
-  // let bulletTimer
-  
-  // // Virus 1
-  // function addDrags(position) {
-  //   gameArray[position].classList.add(dragClass)
-  // }
-  // //remove virus from grid 
-  // function removeDrags(position) {
-  //   gameArray[position].classList.remove(dragClass)
-  // }
-  // addDrags(dragStartposition)
-  // let trueOrFalse = true
-  // let shootingBullet = false
-  // function startGame() {
-  //   resetPoints()
-  //   startButton.disabled = true
-
-  //   setInterval(() => {
-  //     if (dragCurrentPosition === bulletCurrentPosition) {
-  //       console.log('HIT')
-  //       removeBullet(bulletCurrentPosition)
-  //       removeDrags(dragCurrentPosition)
-  //       bullethit()
-  //       clearInterval(dragTimer)
-  //       clearInterval(bulletTimer)
-  //       return
-  //     }
-  //     if (trueOrFalse === true) {
-  //       moveRight()
-  //     }
-  //     if (trueOrFalse === false) {
-  //       moveLeft()
-  //     } 
-  //     return
+    setInterval(() => {
+      if (dragCurrentPosition === bulletCurrentPosition) {
+        console.log('HIT')
+        removeBullet(bulletCurrentPosition)
+        removeDrags(dragCurrentPosition)
+        bullethit()
+        clearInterval(dragTimer)
+        clearInterval(bulletTimer)
+        return
+      }
+      if (trueOrFalse === true) {
+        moveRight()
+      }
+      if (trueOrFalse === false) {
+        moveLeft()
+      } 
+      return
       
     
-  //   }, 1000)
-  //   startButton.disabled = false
+    }, 1000)
+    startButton.disabled = false
     
-  // }
+  }
   
-  // function moveRight() {
-  //   if (dragCurrentPosition >= width * width - 20) {
-  //     removeDrags(dragCurrentPosition)
-  //     console.log('point for virus')
-  //     counter()
-  //     clearInterval(dragTimer)
-  //     return
-  //   }
+  function moveRight() {
+    if (dragCurrentPosition >= width * width - 20) {
+      removeDrags(dragCurrentPosition)
+      console.log('point for virus')
+      counter()
+      clearInterval(dragTimer)
+      return
+    }
     
-  //   console.log('alien', dragCurrentPosition)
-  //   console.log('bullet', bulletCurrentPosition)
-  //   removeDrags(dragCurrentPosition)
-  //   dragCurrentPosition = dragCurrentPosition + 1
-  //   addDrags(dragCurrentPosition)
-  //   if (dragCurrentPosition % width === 0) {
-  //     removeDrags(dragCurrentPosition)
+    console.log('alien', dragCurrentPosition)
+    removeDrags(dragCurrentPosition)
+    dragCurrentPosition = dragCurrentPosition + 1
+    addDrags(dragCurrentPosition)
+    if (dragCurrentPosition % width === 0) {
+      removeDrags(dragCurrentPosition)
   
-  //     dragCurrentPosition = (dragCurrentPosition - 1) + width
-  //     addDrags(dragCurrentPosition)
-  //     clearInterval(dragTimer)
-  //     trueOrFalse = false
-  //   }
+      dragCurrentPosition = (dragCurrentPosition - 1) + width
+      addDrags(dragCurrentPosition)
+      clearInterval(dragTimer)
+      trueOrFalse = false
+    }
     
-  // }
-  // function moveLeft() {
+  }
+  function moveLeft() {
     
-  //   if (dragCurrentPosition >= width * width - 20) {
-  //     removeDrags(dragCurrentPosition)
-  //     console.log('point for virus')
-  //     counter()
-  //     clearInterval(dragTimer)
-  //     return
-  //   }
-  //   removeDrags(dragCurrentPosition)
-  //   dragCurrentPosition = dragCurrentPosition - 1
-  //   addDrags(dragCurrentPosition)
-  //   console.log(dragCurrentPosition)
-  //   if (dragCurrentPosition % width === 0) {
-  //     removeDrags(dragCurrentPosition)
-  //     console.log(dragCurrentPosition)
-  //     dragCurrentPosition += width
-  //     console.log(dragCurrentPosition)
-  //     addDrags(dragCurrentPosition)
-  //     clearInterval(dragTimer)
-  //     trueOrFalse = true
-  //   }
+    if (dragCurrentPosition >= width * width - 20) {
+      removeDrags(dragCurrentPosition)
+      console.log('point for virus')
+      counter()
+      clearInterval(dragTimer)
+      return
+    }
+    removeDrags(dragCurrentPosition)
+    dragCurrentPosition = dragCurrentPosition - 1
+    addDrags(dragCurrentPosition)
+    if (dragCurrentPosition % width === 0) {
+      removeDrags(dragCurrentPosition)
+      dragCurrentPosition += width
+      addDrags(dragCurrentPosition)
+      clearInterval(dragTimer)
+      trueOrFalse = true
+    }
     
-  // }
-  // function counter() {
-  //   lives -= 1
-  //   livesCounter.innerText = lives
-  //   addDrags(dragStartposition)
-  //   console.log(dragCurrentPosition)
-  //   if (lives === 0) {
-  //     console.log('you lost')
+  }
+
+  // LIVES COUNTER
+
+  function counter() {
+    lives -= 1
+    livesCounter.innerText = lives
+    addDrags(dragStartposition)
+    if (lives === 0) {
+      console.log('you lost')
       
-  //   }
-  // }
-  // function bullethit() {
-  //   score += 10
-  //   scoreCounter.innerText = score 
-  // }
-  // console.log(bullethit())
+    }
+  }
+  function bullethit() {
+    score += 10
+    scoreCounter.innerText = score 
+  }
+  console.log(bullethit())
 
-  // function resetPoints() {
-  //   score = 0
-  //   scoreCounter.innerText = score
-  // }
+  function resetPoints() {
+    score = 0
+    scoreCounter.innerText = score
+  }
 
   
-  // // Shooting motion BULLET
-  // // ISSUES: bullet only shoots once
-  // // intervall doesn't seem to stop 
-  // // space bar will also fire the start button again if mouse not moved 
-  // // 
-  // //Add bullet to grid 
-  // function addBullet(position) {
-  //   gameArray[position].classList.add(bullet)
-  // }
+  // SHOOTING 
+
+  function addBullet(position) {
+    gameArray[position].classList.add(bullet)
+  }
   
-  // function removeBullet(position) {
-  //   gameArray[position].classList.remove(bullet)
-  // }
-  // function shootingBulletYet(event) {
-  //   const key = event.keyCode
-  //   if (key === 32) {
-  //     shootingBullet = true
-  //     bulletCurrentPosition = lipCurrentPosition - 20
-  //     shootBullet()
-  //   }
-  //   if (shootingBullet === false) { 
-  //     console.log('do nothing')
-  //   }
-  // }
-  // const bulletStartPosition = lipCurrentPosition - 10
-  // function shootBullet() {
+  function removeBullet(position) {
+    gameArray[position].classList.remove(bullet)
+  }
+  function shootingBulletYet(event) {
+    const key = event.keyCode
+    if (key === 32) {
+      shootingBullet = true
+      bulletCurrentPosition = lipCurrentPosition - 20
+      shootBullet()
+    }
+    if (shootingBullet === false) { 
+      console.log('do nothing')
+    }
+  }
+  const bulletStartPosition = lipCurrentPosition - 10
+  function shootBullet() {
    
-  //   const newInterval = setInterval(() => {
-  //     if (shootingBullet === true) {
+    const newInterval = setInterval(() => {
+      if (shootingBullet === true) {
         
-  //       addBullet(bulletStartPosition)
+        addBullet(bulletStartPosition)
         
-  //     }
-  //     if (bulletCurrentPosition !== bulletStartPosition) {
+      }
+      if (bulletCurrentPosition !== bulletStartPosition) {
         
-  //       removeBullet(bulletStartPosition)
-  //       removeBullet(bulletCurrentPosition + 10)
-  //       addBullet(bulletCurrentPosition)
+        removeBullet(bulletStartPosition)
+        removeBullet(bulletCurrentPosition + 10)
+        addBullet(bulletCurrentPosition)
         
-  //       bulletCurrentPosition = bulletCurrentPosition - 10
-  //     }
-  //     if (bulletCurrentPosition < width) {
+        bulletCurrentPosition = bulletCurrentPosition - 10
+      }
+      if (bulletCurrentPosition < width) {
         
-  //       removeBullet(bulletCurrentPosition + 10)
-  //       console.log('clearing interval') 
-  //       bulletCurrentPosition = lipCurrentPosition - 20
+        removeBullet(bulletCurrentPosition + 10)
+        console.log('clearing interval') 
+        bulletCurrentPosition = lipCurrentPosition - 20
       
-  //       shootingBullet = false
-  //       clearInterval(newInterval)
-  //     }
+        shootingBullet = false
+        clearInterval(newInterval)
+      }
       
-  //   }, 300)     
-  // }
+    }, 300)     
+  }
+
+  // EVENT LISTENERS 
   startButton.addEventListener('click', startGame)
-  // document.addEventListener('keydown', shootingBulletYet)
-  // console.log('key pressed', shootBullet)
-  
+  document.addEventListener('keydown', shootingBulletYet)
+  console.log('key pressed', shootBullet)
   document.addEventListener('keydown', keyDownHandler, false )
   document.addEventListener('keyup', keyUpHandler, false)
 
